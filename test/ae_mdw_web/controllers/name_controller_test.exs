178b948947b2482fc35154bc3bf3abfff948cea8
Mihail Dobrev <mixaildobrev@gmail.com>
2024-04-23 10:27:43 +0300

    fix: fix formatting

--- test/ae_mdw_web/controllers/name_controller_test.exs
+++ test/ae_mdw_web/controllers/name_controller_test.exs
@@ -1707,14 +1707,11 @@ defmodule AeMdwWeb.NameControllerTest do
         |> Enum.reduce(store, fn i, store ->
           plain_name = "#{i}.chain"
 
-          name =
-            Model.name(index: plain_name)
+          name = Model.name(index: plain_name)
 
-          owner =
-            Model.owner(index: {owner_pk.(i), plain_name})
+          owner = Model.owner(index: {owner_pk.(i), plain_name})
 
-          owner_deactivation =
-            Model.owner_deactivation(index: {owner_pk.(i), i + 10, plain_name})
+          owner_deactivation = Model.owner_deactivation(index: {owner_pk.(i), i + 10, plain_name})
 
           store
           |> Store.put(Model.ActiveName, name)
@@ -1729,14 +1726,11 @@ defmodule AeMdwWeb.NameControllerTest do
         |> Enum.reduce(store, fn i, store ->
           plain_name = "#{i}.chain"
 
-          name =
-            Model.name(index: plain_name)
+          name = Model.name(index: plain_name)
 
-          owner =
-            Model.owner(index: {owner_pk.(i), plain_name})
+          owner = Model.owner(index: {owner_pk.(i), plain_name})
 
-          owner_deactivation =
-            Model.owner_deactivation(index: {owner_pk.(i), i + 10, plain_name})
+          owner_deactivation = Model.owner_deactivation(index: {owner_pk.(i), i + 10, plain_name})
 
           store
           |> Store.put(Model.InactiveName, name)
